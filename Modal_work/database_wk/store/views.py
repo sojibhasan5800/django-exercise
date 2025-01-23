30460bbc1e1ec92b80ca2ea9a549f2f7f33f7294
phitron-demo <sojibhasan5800@gmail.com>
2025-01-23 07:54:51 +0600

    f

--- Modal_work/database_wk/store/views.py
+++ Modal_work/database_wk/store/views.py
@@ -1,8 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
 # Create your views here.
 from .models import Student
 def homepage(request):
+    data = Student.objects.all()
+    return render(request,'abc/home.html',{'data':data})
+def deletedata(request,roll):
+    Student.objects.get(pk = roll).delete()
+    
     data = Student.objects.all()
     return render(request,'abc/home.html',{'data':data})
 
