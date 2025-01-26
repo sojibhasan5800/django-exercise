62e7611eef39e1b587741fa193f287eefd35c18f
phitron-demo <sojibhasan5800@gmail.com>
2025-01-26 10:58:18 +0600

    Removed folder_name from repository

--- catagories/urls.py
+++
@@ -1,5 +0,0 @@
-from django.urls import path,include
-from .views import catagoripage
-urlpatterns = [
- path('page/',catagoripage,name="catagori_page")
-]
--- profiles/urls.py
+++
@@ -1,5 +0,0 @@
-from django.urls import path,include
-from .views import profilepage
-urlpatterns = [
- path('page/',profilepage,name="profile_page")
-]
