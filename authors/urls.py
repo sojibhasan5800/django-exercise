2cb32924ae540b616695bf98926f5ecd69bcc2ec
phitron-demo <sojibhasan5800@gmail.com>
2025-01-26 10:01:31 +0600

    Removed folder_name from repository

--- authors/urls.py
+++
@@ -1,6 +0,0 @@
-
-from django.urls import path,include
-from .views import authorpage
-urlpatterns = [
- path('page/',authorpage,name="author_page")
-]
--- posts/urls.py
+++
@@ -1,5 +0,0 @@
-from django.urls import path,include
-from .views import postpage
-urlpatterns = [
- path('page/',postpage,name="post_page")
-]
