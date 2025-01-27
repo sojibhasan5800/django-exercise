fdb5bc7ecaa5c6ea9528392282dbfcdfa5d5748a
sojibhasan580 <sojibhasan5800@gmail.com>
2025-01-27 13:11:43 +0600

    d

--- blog_project/posts/urls.py
+++ blog_project/posts/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path,include
-from .views import postpage
+from .views import postpage,editpost,deletepost
 urlpatterns = [
- path('page/',postpage,name="post_page")
+ path('page/',postpage,name="post_page"),
+ path('edit/<int:id>/',editpost,name="edit_page"),
+ path('delete/<int:id>/',deletepost,name="delete_page")
 ]
