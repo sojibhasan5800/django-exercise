96af76e2680ccc8550e5aec3ecdb94eb5d738b0e
phitron-demo <sojibhasan5800@gmail.com>
2025-02-13 10:05:56 +0600

    f

--- blog_project2/blog_project/views.py
+++ blog_project2/blog_project/views.py
@@ -4,7 +4,8 @@ from catagories.models import catagori
 def homepage(request,category_slug=None):
     data = post.objects.all()
     if category_slug is not None:
-        category_data =  catagori.objects.get(slug= category_slug)
-        data = post.objects.filter()
+        category_data =  catagori.objects.get(slug = category_slug)
+        data = post.objects.filter(category = category_data)
+    categories = catagori.objects.all()
    
-    return render(request,'home.html',{'data':data , 'catagory': catagory_data})
+    return render(request,'home.html',{'data':data , 'category': categories})
